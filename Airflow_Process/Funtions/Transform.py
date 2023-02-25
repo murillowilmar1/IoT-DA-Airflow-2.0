026e4fa437878b33dfbff21c572c0d68ab4334d2
murillowilmar1 <murillowilmar1@homtail.com>
2023-02-25 11:29:10 -0500

    Trans

--- Airflow_Process/Funtions/Transform.py
+++ Airflow_Process/Funtions/Transform.py
@@ -1,6 +1,7 @@
 
 
 import pandas as pd 
+import numpy as np 
 
 
 def transf():
@@ -8,6 +9,18 @@ def transf():
 
      url= "/usr/local/airflow/tests/arduino_table.csv"
      df=pd.read_csv(url)
+
+
+     # Variable Temperatura 
+     mean =df.temperatura.mean()
+     df["temperatura"]= df["temperatura"].replace(np.nan, mean)
+
+     
+     # Variable Humedad  
+     mean =df.hum.mean()
+     df["hum"]= df["hum"].replace(np.nan, mean)
+
+
 
 
      
